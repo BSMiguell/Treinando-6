f90bd7cac980c9169201a2e38318410071365440
BSMiguell <bruno.s.miiguel@gmail.com>
2023-06-28 05:52:27 -0300

    Finalizando a Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,20 @@
 import React from "react";
 import { motion } from "framer-motion";
 
+const Skill = ({ name, x, y }) => {
+  return (
+    <motion.div
+      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute"
+      whileHover={{ scale: 1.05 }}
+      initial={{ x: 0, y: 0 }}
+      whileInView={{ x: x, y: y }}
+      transition={{ duration: 1.5 }}
+    >
+      {name}
+    </motion.div>
+  );
+};
+
 const Skills = () => {
   return (
     <>
@@ -12,6 +26,18 @@ const Skills = () => {
         >
           Web
         </motion.div>
+
+        <Skill name="HTML" x="-25vw" y="2vw" />
+        <Skill name="Django" x="-32vw" y="-5vw" />
+        <Skill name="CSS" x="-5v" y="-10vw" />
+        <Skill name="Javascript" x="20vw" y="6vw" />
+        <Skill name="ReactJS" x="0vw" y="12vw" />
+        <Skill name="NextJS" x="-20vw" y="-15vw" />
+        <Skill name="SASS" x="15vw" y="-12vw" />
+        <Skill name="Web Design" x="32vw" y="-5vw" />
+        <Skill name="Figma" x="0vw" y="-20vw" />
+        <Skill name="VueJS" x="-25vw" y="18vw" />
+        <Skill name="Tailwind CSS" x="18vw" y="18vw" />
       </div>
     </>
   );
