e77a65c8f00a5f79cd62c30837003178b5397381
BSMiguell <bruno.s.miiguel@gmail.com>
2023-06-28 05:55:39 -0300

    Teste

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -4,11 +4,7 @@ import Link from "next/link";
 
 const HireMe = () => {
   return (
-    <div
-      className="fixed left-4 bottom-4
-flex items-center justify-center overflow-hidden
-"
-    >
+    <div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden ">
       <div className="w-48 h-auto flex items-center justify-center relative">
         <CircularText className={"fill-dark animate-spin-slow"} />
 
