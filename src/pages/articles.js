f96bc2734219995784c479c13f9c34f2f89f9ce6
BSMiguell <bruno.s.miiguel@gmail.com>
2023-06-29 09:45:06 -0300

    Craindo a Articles.js

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -3,16 +3,76 @@ import Layout from "@/components/Layout";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
 import article1 from "../../public/images/articles/pagination component in reactjs.jpg";
 import article2 from "../../public/images/articles/create loading screen in react js.jpg";
-import { motion } from "framer-motion";
+import { motion, useMotionValue } from "framer-motion";
+import article3 from "../../public/images/articles/create modal component in react using react portals.png";
+import article4 from "../../public/images/articles/form validation in reactjs using custom react hook.png";
+import article5 from "../../public/images/articles/smooth scrolling in reactjs.png";
 
 const FramerImage = motion(Image);
 
+const MovingImg = ({ title, img, link }) => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const imgRef = useRef(null);
+
+  function handleMouse(event) {
+    imgRef.current.style.display = "inline-block";
+    x.set(event.pageX);
+    y.set(-10);
+  }
+  function handleMouseLeave(event) {
+    imgRef.current.style.display = "none";
+    x.set(0);
+    y.set(0);
+  }
+
+  return (
+    <Link
+      href={link}
+      target="_blank"
+      onMouseMove={handleMouse}
+      onMouseLeave={handleMouseLeave}
+    >
+      <h2 className="capitalize text-x1 font-semibold hover:underline">
+        {title}
+      </h2>
+
+      <FramerImage
+        style={{ x: x, y: y }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1, transition: { duration: 1 } }}
+        ref={imgRef}
+        src={img}
+        alt={title}
+        className="z-10 w-96 h-auto hidden absolute rounded-lg"
+      />
+    </Link>
+  );
+};
+
+const Article = ({ img, title, date, link }) => {
+  return (
+    <motion.li
+      initial={{ y: 200 }}
+      whileInView={{ y: 0, transition: { duration: 0.5, ease: "easeInOut" } }}
+      viewport={{ once: true }}
+      className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4"
+    >
+      <MovingImg title={title} img={img} link={link} />
+
+      <span className="text-primary font-semibold pl-4">{date}</span>
+    </motion.li>
+  );
+};
+
 const FeaturedArticle = ({ img, title, time, summary, link }) => {
   return (
-    <li className="col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl">
+    <li className="relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl" />
+
       <Link
         href={link}
         target="_blank"
@@ -28,7 +88,7 @@ const FeaturedArticle = ({ img, title, time, summary, link }) => {
       </Link>
 
       <Link href={link} target="_blank">
-        <h2 className="capitalize text-2xl font-bold my-2 hover:underline">
+        <h2 className="capitalize text-2xl font-bold my-2 mt-4 hover:underline">
           {title}
         </h2>
       </Link>
@@ -39,7 +99,7 @@ const FeaturedArticle = ({ img, title, time, summary, link }) => {
   );
 };
 
-const articles = () => {
+const ArticlesPage = () => {
   return (
     <>
       <Head>
@@ -68,10 +128,66 @@ const articles = () => {
               img={article2}
             />
           </ul>
+
+          <h2 className="font-bold text-4xl w-full text-center my-16 mt-32">
+            All Articles
+          </h2>
+
+          <ul>
+            <Article
+              link="/"
+              title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling "
+              date="March 22, 2023"
+              img={article3}
+            />
+            <Article
+              link="/"
+              title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling "
+              date="March 22, 2023"
+              img={article4}
+            />
+            <Article
+              link="/"
+              title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling "
+              date="March 22, 2023"
+              img={article5}
+            />
+            <Article
+              link="/"
+              title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling "
+              date="March 22, 2023"
+              img={article3}
+            />
+
+            <Article
+              link="/"
+              title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling "
+              date="March 22, 2023"
+              img={article3}
+            />
+            <Article
+              link="/"
+              title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling "
+              date="March 22, 2023"
+              img={article3}
+            />
+            <Article
+              link="/"
+              title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling "
+              date="March 22, 2023"
+              img={article3}
+            />
+            <Article
+              link="/"
+              title="Form Validation In Reactjs: Build A Reusable Custom Hook For Inputs And Error Handling "
+              date="March 22, 2023"
+              img={article3}
+            />
+          </ul>
         </Layout>
       </main>
     </>
   );
 };
 
-export default articles;
+export default ArticlesPage;
