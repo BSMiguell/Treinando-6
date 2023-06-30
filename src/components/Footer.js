7a89d378a92418de70e70f24f94edee8cb81088f
BSMiguell <bruno.s.miiguel@gmail.com>
2023-06-30 06:37:14 -0300

    Deixando o Footer Responsivel

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light">
-      <Layout className="py-8 flex items-center justify-between ">
+    <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light sm:text-base">
+      <Layout className="py-8 flex items-center justify-between lg:flex-col lg:py-6">
         <span>{new Date().getFullYear()}&copy; All Rights reserved. </span>
-        <div className="flex items-center">
+        <div className="flex items-center lg:py-2">
           Build With{" "}
           <span className="text-primary dark:text-primaryDark text-2x1 px-1">
             &#9825;
