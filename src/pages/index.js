73f9fe69701426ebf23a1dbb851c733172953494
BSMiguell <bruno.s.miiguel@gmail.com>
2023-06-30 00:42:14 -0300

    Teste

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,13 @@ export default function Home() {
         <Layout className="pt-0">
           <div className="flex items-center justify-between w-full">
             <div className="w-1/2">
-              <Image src={profilePic} alt="BSM" className="w-full h-auto" />
+              <Image
+                src={profilePic}
+                alt="BSM"
+                className="w-full h-auto"
+                priority
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+              />
             </div>
 
             <div className="w-1/2 flex flex-col items-center self-center">
