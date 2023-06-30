500a88c8b2722e096c361605f0fe464a0b9ab865
BSMiguell <bruno.s.miiguel@gmail.com>
2023-06-30 06:34:26 -0300

    Deixando Home Responsivel

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,39 +17,40 @@ export default function Home() {
       </Head>
 
       <main className="flex items-center text-dark w-full min-h-screen dark:text-light">
-        <Layout className="pt-0">
-          <div className="flex items-center justify-between w-full">
-            <div className="w-1/2">
+        <Layout className="pt-0 md:pt-16 sm:pt-8">
+          <div className="flex items-center justify-between w-full lg:flex-col">
+            <div className="w-1/2 md:w-full">
               <Image
                 src={profilePic}
                 alt="BSM"
-                className="w-full h-auto"
+                className="w-full h-auto lg:hidden md:inline-block md:w-full"
                 priority
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
               />
             </div>
 
-            <div className="w-1/2 flex flex-col items-center self-center">
+            <div className="w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center">
               <AnimatedText
                 text="Turning Vision Into Reality With Code And Design."
-                className="!text-6xl text-left"
+                className="!text-6xl text-left 
+                xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
 
-              <p className="my-4 text-base font-medium">
+              <p className="my-4 text-base font-medium md:text-sm sm:text-xs">
                 As a skilled full-stack developer, I am dedicated to turning
                 ideas into innovative web applications. Explore my latest
                 projects and articles, showcasing my expertise in React.js and
                 web development.
               </p>
 
-              <div className="flex items-center self-start mt-2">
+              <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
                   href="/BrunoSilvaMiguel.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
                   border-2 border-solid border-transparent hover:border-dark 
                   dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light
-                  hover:dark:border-light"
+                  hover:dark:border-light md:p-2 md:px-4 md:text-base"
                   download={true}
                 >
                   Resume <LinkArrow className={"w-6 ml-1"} />
@@ -59,7 +60,7 @@ export default function Home() {
                   href="BSM: swainsegundo@gmail.com"
                   target={"_blank"}
                   className="ml-4 text-lg font-medium capitalize text-dark
-                  underline dark:text-light"
+                  underline dark:text-light md:text-base"
                 >
                   Contact
                 </Link>
@@ -70,7 +71,7 @@ export default function Home() {
 
         <HireMe />
 
-        <div className="absolute right-8 bottom-8 inline-block w-24">
+        <div className="absolute right-8 bottom-8 inline-block w-24 md:hidden">
           <Image src={lightBulb} alt="BSM" className="w-full h-auto" />
         </div>
       </main>
